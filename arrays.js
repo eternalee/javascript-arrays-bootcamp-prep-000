659f7034d8f59ed264a674e05f275fddaa737620
Eterna <one.eternity@gmail.com>
2017-12-28 23:28:35 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var array = ["Happy", "Sad"];
+
+console.log(array);
+
+function addElementToBeginningOfArray(element) {
+  [element, ...array]
+}
+addElementToBeginningOfArray("hi");
+console.log(array)
+
+function addElementDestructivelyToBeginningOfArray(element) {
+  array = [element, ...array]
+}
+addElementDestructivelyToBeginningOfArray("hi")
+console.log(array);
+
+
+var cats = ["milo", "garfield", "otis"]
+function removeElementFromBeginningOfArray(x) {
+  cats = cats.slice(x)
+}
+removeElementFromBeginningOfArray(1)
+console.log(cats)
+
+var iceCreams = ["cho", "strawb", "rockyroad"]
+console.log(iceCreams)
+function destructivelyRemoveElementFromEndOfArray() {
+  iceCreams.pop()
+}
+destructivelyRemoveElementFromEndOfArray()
+console.log(iceCreams)
