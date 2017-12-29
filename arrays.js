86c0129e8f622415b01e6afbf5feceff7a06f327
Eterna <one.eternity@gmail.com>
2017-12-29 01:11:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,19 +2,19 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 var array = [];
 
-function addElementToBeginningOfArray(array, element) {
-  [x, ...array]
+function addElementToBeginningOfArray(array, x) {
+  return [x, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(x, array) {
-  array = [x, ...array]
+function destructivelyAddElementToBeginningOfArray(array, x) {
+  return array = [x, ...array]
 }
 
-function addElementToEndOfArray(x, array) {
+function addElementToEndOfArray(array, x) {
   [...array, x]
 }
 
-function destructivelyAddElementToEndOfArray(x, array) {
+function destructivelyAddElementToEndOfArray(array, element) {
   array = [...array, x]
 }
 console.log(array)
