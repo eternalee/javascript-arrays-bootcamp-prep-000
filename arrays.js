492ef51d546fad5ae6eef585b8e6be873460b0a6
Eterna <one.eternity@gmail.com>
2017-12-29 00:29:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,20 +15,3 @@ function destructivelyAddElementToBeginningOfArray(x) {
 addElementDestructivelyToBeginningOfArray("hi")
 console.log(chocolateBars)
 
-//////////////////////////////
-
-
-var cats = ["milo", "garfield", "otis"]
-function removeElementFromBeginningOfArray(x) {
-  cats = cats.slice(x)
-}
-removeElementFromBeginningOfArray(1)
-console.log(cats)
-
-var iceCreams = ["cho", "strawb", "rockyroad"]
-console.log(iceCreams)
-function destructivelyRemoveElementFromEndOfArray() {
-  iceCreams.pop()
-}
-destructivelyRemoveElementFromEndOfArray()
-console.log(iceCreams)
